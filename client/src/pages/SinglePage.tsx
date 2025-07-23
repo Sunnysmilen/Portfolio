4a13dbc8d1e72c4e57912f70c386f69872ca3d4a
sunnysmilen <davidsalnicole@gmail.com>
2025-07-23 11:31:53 +0200

    feat: creation single page

---
+++ client/src/pages/SinglePage.tsx
@@ -0,0 +1,50 @@
+
+export default function SinglePage() {
+  return (
+    <>
+
+      <div className="container">
+        <section id="firstsection">
+          <h1>First Section</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis
+            optio error commodi distinctio! Accusantium obcaecati, voluptatum
+            quo inventore voluptatem officiis dicta, quae, quisquam dolor
+            reiciendis suscipit. Voluptatibus illum repellat aperiam.
+          </p>
+           
+        </section>
+        <section id="secondsection">
+          <h1> Second Section</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis
+            optio error commodi distinctio! Accusantium obcaecati, voluptatum
+            quo inventore voluptatem officiis dicta, quae, quisquam dolor
+            reiciendis suscipit. Voluptatibus illum repellat aperiam.
+          </p>
+          
+        </section>
+        <section id="thirdsection">
+          <h1>Third Section</h1>
+          Third Section
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis
+            optio error commodi distinctio! Accusantium obcaecati, voluptatum
+            quo inventore voluptatem officiis dicta, quae, quisquam dolor
+            reiciendis suscipit. Voluptatibus illum repellat aperiam.
+          </p> 
+        </section>
+        <section id="fourthsection">
+          <h1>Fourth Section</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis
+            optio error commodi distinctio! Accusantium obcaecati, voluptatum
+            quo inventore voluptatem officiis dicta, quae, quisquam dolor
+            reiciendis suscipit. Voluptatibus illum repellat aperiam.
+          </p>
+          
+        </section>
+      </div>
+    </>
+  );
+}
