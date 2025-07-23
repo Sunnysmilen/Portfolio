700f481db9b2605cbeafaf23fb588ea32960932f
sunnysmilen <davidsalnicole@gmail.com>
2025-07-23 13:11:54 +0200

    feat: creation of technologies page

---
+++ client/src/components/Cards/TechnologiesCard.tsx
@@ -0,0 +1,10 @@
+export default function TechnologiesCard() {
+  return (
+    <>
+      <section>
+        <img src="#" alt="Logo de la technologie" />
+        <p>nom de techno</p>
+      </section>
+    </>
+  );
+}
--- client/src/pages/SinglePage.tsx
+++ client/src/pages/SinglePage.tsx
@@ -1,6 +1,7 @@
 import Homepage from "../components/SinglePage/Homepage";
 import Presentation from "../components/SinglePage/Presentation";
 import Project from "../components/SinglePage/Project";
+import Technologies from "../components/SinglePage/Technologies";
 
 export default function SinglePage() {
   return (
@@ -9,15 +10,7 @@ export default function SinglePage() {
         <Homepage />
         <Presentation />
         <Project />
-        <section id="fourthsection">
-          <h1>Fourth Section</h1>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis
-            optio error commodi distinctio! Accusantium obcaecati, voluptatum
-            quo inventore voluptatem officiis dicta, quae, quisquam dolor
-            reiciendis suscipit. Voluptatibus illum repellat aperiam.
-          </p>
-        </section>
+        <Technologies />
       </div>
     </>
   );
