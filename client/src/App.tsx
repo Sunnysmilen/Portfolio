1161133ac471196e7a1cc5e6793d980b1e6a1152
sunnysmilen <davidsalnicole@gmail.com>
2025-07-23 12:02:33 +0200

    feat: creation Homepage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
+import { Outlet } from "react-router";
 import "./App.css";
 import { Navbar } from "./components/Navbar";
-import SinglePage from "./pages/SinglePage";
 
 function App() {
   return (
     <>
-      <h1>Mon Portfolio</h1>
-      <Navbar/>
-      <SinglePage />
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
     </>
   );
 }
---
+++ client/src/pages/Homepage.tsx
@@ -0,0 +1,15 @@
+import "../assets/styles/homepage.css";
+
+export default function Homepage() {
+  return (
+    <>
+      <section id="homepage" className="Home">
+        <h1>Nicole DAVID</h1>
+        <h2>Développeuse Web</h2>
+        <p>Découvrez mon parcours de développeuse web</p>
+        <p>Disponible</p>
+        <button type="button">Contactez-moi</button>
+      </section>
+    </>
+  );
+}
