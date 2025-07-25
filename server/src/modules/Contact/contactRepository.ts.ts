d5773f286f695d88fd5249d0c926debfc2a77f5e
sunnysmilen <davidsalnicole@gmail.com>
2025-07-25 11:15:03 +0200

    feat: created contact form

---
+++ server/src/modules/Contact/contactRepository.ts.ts
@@ -0,0 +1,13 @@
+import databaseClient, { type Rows } from "../../../database/client";
+import type { candidatType } from "../../Definitions/Candidat";
+
+class contactRepository {
+  async readAll() {
+    const [rows] =
+      await databaseClient.query<Rows>(`SELECT candidat.*,statut.name AS name, statut.description AS introduction  FROM candidat
+        INNER JOIN statut ON candidat.statut_id = statut_id`);
+    return rows as candidatType[];
+  }
+}
+
+export default new contactRepository();
